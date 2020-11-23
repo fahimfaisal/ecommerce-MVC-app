14e34a821ff8b355f7e5d8c9124f272f477348da
fahimfaisal <navid2010ps@gmail.com>
2020-11-23 16:33:17 +1100

    Created a get action and post action method

--- Areas/Admin/Controllers/ProductTypesController.cs
+++ Areas/Admin/Controllers/ProductTypesController.cs
@@ -1,4 +1,5 @@
 ﻿using ecommerce.Data;
+using ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -15,11 +16,37 @@ namespace ecommerce.Areas.Admin.Controllers
 
         public ProductTypesController(ApplicationDbContext db)
         {
-            _db = db;       
+            _db = db;
         }
         public IActionResult Index()
         {
             return View(_db.ProductTypes.ToList());
         }
+
+        //Create GetAction Method
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Create PostAction Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult>Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ProductTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(actionName: nameof(Index));
+
+            }
+
+            return View(productTypes);
+        }
+
     }
 }
